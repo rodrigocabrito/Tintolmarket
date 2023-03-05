885547a4f2ff7c05e8860112b909e88af3f8c5ec
Rodrigo Cabrito <rodri.cabrito@gmail.com>
2023-03-05 14:24:32 +0000

    Implementing skeleton of basic Client and Server

--- main/TintolmarketClient.java
+++ main/TintolmarketClient.java
@@ -1,5 +1,32 @@
 package Tintolmarket.main;
 
+import java.net.Socket;
+
+/*
+ * @authors:
+ *      Rodrigo Cabrito 54455
+ *      João Costa 54482
+ *      João Fraga 44837
+ */
+
 public class TintolmarketClient {
     
+    private static int port = 9999;
+
+    public static void main(String[] args) {
+        
+        try {
+            
+            Socket clientSocket = new Socket("",port);
+
+            //streams
+
+            //write/read to/from server
+
+            //close streams and socket
+
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
 }
--- main/TintolmarketServer.java
+++ main/TintolmarketServer.java
@@ -1,5 +1,69 @@
 package Tintolmarket.main;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/*
+ * @authors:
+ *      Rodrigo Cabrito 54455
+ *      João Costa 54482
+ *      João Fraga 44837
+ */
+
 public class TintolmarketServer {
     
+    private static int port;
+
+    public static void main(String[] args) throws IOException {
+        
+        ServerSocket serverSocket = null;
+
+        try {
+
+            serverSocket = new ServerSocket(port);
+
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+            System.exit(-1);
+        }
+
+        while(true) {
+
+            Socket clientSocket = null;
+
+			try {
+				clientSocket = serverSocket.accept();
+				clientHandlerThread clientThread = new clientHandlerThread(clientSocket);
+                clientThread.start();
+		    }
+		    catch (IOException e) {
+		        e.printStackTrace();
+		    }
+
+        //serverSocket.close();
+        }
+    }
+
+
+    /*
+     * classe para criar threads para vários clientes comunicarem com o servidor
+     */
+    static class clientHandlerThread extends Thread {
+
+        private Socket socket = null;
+
+        clientHandlerThread(Socket socket) {
+            this.socket = socket;
+        }
+
+        
+        public void run() {
+            //streams
+
+            //read/write from/to client
+
+            //close streams and socket
+        }
+    }
 }
