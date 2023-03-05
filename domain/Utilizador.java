b8708a6f879cfb702600bac7bfcf1b23d6f9e8f9
Rodrigo Cabrito <rodri.cabrito@gmail.com>
2023-03-05 16:19:59 +0000

    Implementing Utilizador

--- domain/Utilizador.java
+++ domain/Utilizador.java
@@ -1,5 +1,30 @@
 package Tintolmarket.domain;
 
+/*
+ * @authors:
+ *      Rodrigo Cabrito 54455
+ *      João Costa 54482
+ *      João Fraga 44837
+ */
+
 public class Utilizador {
     
+    private String id = null;
+    private int wallet = 200;
+
+    public Utilizador(String id) {
+        this.id = id;
+    }
+
+    public void updateWallet(int change) {
+        this.wallet += change;
+    }
+
+    public String getUserID() {
+        return this.id;
+    }
+
+    public int getBalance() {
+        return this.wallet;
+    }
 }
