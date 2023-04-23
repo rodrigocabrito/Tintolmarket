5ac8820ae27bcb275304e8d2c79ca63b6a90178f
Rodrigo Cabrito <rodri.cabrito@gmail.com>
2023-04-23 22:48:31 +0100

    adicionado metodo para escrever os dados de um bloco num ficheiro .blk, retificação na verificação da validez da chain, adicionado metodo para converter ficheiros .blk em blocos numa blockchain e alteração no metodo de adicionar transação para tratar das necessidades de criar novos blocos e enviar a informção dos blocos para os ficheiros .blk correspondentes.

--- src/Blockchain.java
+++ src/Blockchain.java
@@ -1,9 +1,12 @@
+import java.io.*;
+import java.security.Signature;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Blockchain {
 
     private final List<Block> blocks;
+    private final String BLOCKCHAIN_DIR = "./blockchain/";
 
     public Blockchain() {
         this.blocks = new ArrayList<>();
@@ -12,8 +15,18 @@ public class Blockchain {
 
     public void addBlock(Block block) {
         Block previousBlock = blocks.get(blocks.size() - 1);
-        block.setHash(previousBlock.getHash());
+        block.setHash(previousBlock.calculateHash());
         blocks.add(block);
+    }
+
+    public void updateBlockFile(Block block) throws IOException {
+        // Write the block data to a new .blk file
+        File blkFile = new File(BLOCKCHAIN_DIR, "block_" + block.getId() + ".blk");
+        DataOutputStream outputStream = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(blkFile)));
+        byte[] blockData = block.toByteArray();
+        outputStream.writeInt(blockData.length);
+        outputStream.write(blockData);
+        outputStream.close();
     }
 
     public boolean isChainValid() {
@@ -21,8 +34,10 @@ public class Blockchain {
             Block currentBlock = blocks.get(i);
             Block previousBlock = blocks.get(i - 1);
 
-            if (!currentBlock.getHash().equals(currentBlock.calculateHash())) {
-                System.out.println("Block " + i + " has been tampered with.");
+            currentBlock.setHash(previousBlock.getHash());
+
+            if (!currentBlock.getHash().equals(previousBlock.calculateHash())) {
+                System.out.println("Bloco com id " + i + " tem hash errado.");
                 return false;
             }
 
@@ -34,8 +49,48 @@ public class Blockchain {
         return true;
     }
 
-    public void addTransacao(Transacao transacao) {
-        this.blocks.get(blocks.size() - 1).addTransacao(transacao);
+    public void loadBlocks() {
+        // Load all the .blk files from the blocks directory
+        File blkDir = new File("blockchain");
+        File[] blkFiles = blkDir.listFiles((BLOCKCHAIN_DIR, name) -> name.endsWith(".blk"));
+        if (blkFiles != null) {
+            // Read each .blk file and create a Block object from it
+            for (File blkFile : blkFiles) {
+                try {
+                    RandomAccessFile file = new RandomAccessFile(blkFile, "r");
+
+                    // Read the block data from the file
+                    int blockDataLength = file.readInt();
+                    byte[] blockData = new byte[blockDataLength];
+                    file.readFully(blockData);
+
+                    // Create a Block object from the data
+                    Block block = Block.fromByteArray(blockData);
+
+                    // Add the block to the blockchain
+                    blocks.add(block);
+
+                    file.close();
+                } catch (IOException e) {
+                    // Handle the exception
+                }
+            }
+        }
+    }
+
+    public void addTransacao(Transacao transacao, Signature signature) throws IOException {
+        Block lastBlock = getLastBlock();
+        lastBlock.addTransacao(transacao);
+        updateBlockFile(getLastBlock());
+
+        if (isLastBlockFull()) {
+            lastBlock.closeBlock(signature);
+            updateBlockFile(getLastBlock());
+            this.blocks.add(new Block(lastBlock.getId() + 1));
+            lastBlock = getLastBlock();
+            Block previousLastBlock = blocks.get(blocks.size() - 2);
+            lastBlock.setHash(previousLastBlock.calculateHash());
+        }
     }
 
     public boolean isLastBlockFull() {
@@ -44,5 +99,9 @@ public class Blockchain {
 
     public Block getLastBlock() {
         return this.blocks.get(blocks.size() - 1);
+    }
+
+    public List<Block> getBlocks() {
+        return this.blocks;
     }
 }
